8d11f1c883dbbd47a0e2a2817370f3a98c9410fc
Ifiok Jr. <ifiokotung@gmail.com>
2026-09-05 17:28:14 +0100

    fix: make database migrations reversible

--- migrations/20241208213425_initial.down.sql
+++ migrations/20241208213425_initial.down.sql
@@ -1,7 +1,9 @@
-DROP TABLE IF EXISTS games;
+DROP TABLE IF EXISTS section_events;
 
 DROP TABLE IF EXISTS sections;
 
-DROP TABLE IF EXISTS section_events;
-
 DROP TABLE IF EXISTS players;
+
+DROP TABLE IF EXISTS encrypted_keypairs;
+
+DROP TABLE IF EXISTS games;
